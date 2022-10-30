51cc3515e9123231f2f9447db87d4f79adbd7613
Dario Palminio <dario.palminio@gmail.com>
2022-10-30 17:45:20 -0300

    test: remove old tests

---
+++ src/__test__/infra/database/repository/ProductRepositoryNegativeStub.ts
@@ -0,0 +1,42 @@
+import { Product } from "src/domain/model/product/product";
+import { IRepository } from "src/domain/outgoing/repository.interface";
+
+// Mocking repository
+export class ProductRepositoryNegativeStub implements IRepository<Product> {
+    findExcludingFields(query: any, fieldsToExclude: any, page?: number, limit?: number, orderByField?: string, isAscending?: boolean): Promise<any[]> {
+        throw new Error('Method not implemented.');
+    }
+    count(query: any): Promise<number> {
+        throw new Error('Method not implemented.');
+    }
+    async getAll(): Promise<Product[]> {
+        return [];
+    };
+    async find(query: any): Promise<Product[]> {
+        return [];
+    }
+    async getById(id: string): Promise<Product> {
+        return null;
+    };
+    async getByQuery(query: any): Promise<Product> {
+        return null;
+    }
+    async hasById(id: string): Promise<boolean> {
+        return false;
+    }
+    async hasByQuery(query: any): Promise<boolean> {
+        return false;
+    }
+    async create<IProduct>(product: IProduct): Promise<Product> {
+        return null;
+    };
+    async updateById(id: string, product: Product): Promise<boolean> {
+        return false;
+    };
+    async update(query: any, valuesToSet: any): Promise<boolean> {
+        return false;
+    };
+    async delete(id: string): Promise<boolean> {
+        return false;
+    };
+};
